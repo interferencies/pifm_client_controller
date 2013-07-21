0a954845d00e94f316d47643d832844278c91b51
Siroco <siroco@riseup.net>
2013-07-22 00:35:44 +0200

    Modificaciones para la traducción

--- gen/net/lakaxita/pifm_controller/R.java
+++ gen/net/lakaxita/pifm_controller/R.java
@@ -45,7 +45,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int arrancar=0x7f050005;
+        public static final int cambiar_frecuencia=0x7f050007;
+        public static final int conectar=0x7f050002;
+        public static final int desconectar=0x7f050003;
+        public static final int info=0x7f050006;
+        public static final int parar=0x7f050004;
     }
     public static final class style {
         /** 
